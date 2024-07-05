0f0f543b624bc1ae4ccaaf11f2f30b9458e9a319
fishphobiagg <dlauddn6211@icloud.com>
2024-07-06 03:24:52 +0900

    검색 api 개발

--- app/crud.py
+++ app/crud.py
@@ -1,22 +1,30 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import text
-from app.models import ProhibitedItem
+from app.models import ProhibitedItem, SearchHistory
 from app.schemas import ProhibitedItemCreate
+import asyncio
 
-def get_prohibited_item_by_name(db: Session, item_name: str):
-    return db.query(ProhibitedItem).filter(ProhibitedItem.item_name == item_name).first()
-
-def get_prohibited_items(db: Session, skip: int = 0, limit: int = 10):
-    return db.query(ProhibitedItem).offset(skip).limit(limit).all()
-
-def create_prohibited_item(db: Session, item: ProhibitedItemCreate):
+async def create_prohibited_item(db: Session, item: ProhibitedItemCreate):
     db_item = ProhibitedItem(**item.model_dump())
     db.add(db_item)
-    db.commit()
-    db.refresh(db_item)
+    await asyncio.to_thread(db.commit)
+    await asyncio.to_thread(db.refresh, db_item)
     return db_item
 
 def search_prohibited_items(db: Session, query: str):
     return db.query(ProhibitedItem).filter(
         text("search_vector @@ plainto_tsquery('english', :query)")
-    ).params(query=query).all()
+    ).params(query=query).limit(10).all()
+
+async def create_search_history(db: Session, search_term: str, prohibited_item_id: int = None):
+    search_history = SearchHistory(search_term=search_term, prohibited_item_id=prohibited_item_id)
+    db.add(search_history)
+    await asyncio.to_thread(db.commit)
+    await asyncio.to_thread(db.refresh, search_history)
+    return search_history
+
+def get_prohibited_item_by_id(db: Session, id: int) -> ProhibitedItem:
+    return db.query(ProhibitedItem).filter(ProhibitedItem.id == id).first()
+
+def get_prohibited_item_by_name(db: Session, name: str):
+    return db.query(ProhibitedItem).filter(text("search_vector @@ plainto_tsquery('english', :name)")).params(name=name).first()
